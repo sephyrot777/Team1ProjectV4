ed85fc27711c173ad38e8de55baf217df4f06748
KimJinHwan <wlsghks0616@naver.com>
2022-07-15 09:58:39 +0900

    Media 수정완료

--- board/urls.py
+++ board/urls.py
@@ -9,5 +9,8 @@ from . import views
 urlpatterns = [
     path('news/', views.NewsView.as_view(), name='news'),
     path('announce/', views.AnnounceView.as_view(), name='announce'),
-    path('teamplayer/', views.TeamPlayerView.as_view(), name='teamPlayer'),
+    path('teamplayer/', views.TeamplayerView.as_view(), name='teamplayer'),
+    path('news_detail/', views.News_detailView.as_view(), name='news_detail'),
+    path('announce_detail/', views.Announce_detailView.as_view(), name='announce_detail'),
+    path('teamplayer_detail/', views.Teamplayer_detailView.as_view(), name='teamplayer_detail'),
 ]
